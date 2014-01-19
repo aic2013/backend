be0142560b847e6d76bde40bcb0851fa501afeae
Markus Mühlberger <mmuehlberger@me.com>
2014-01-19 18:02:35 +0100

    Updated fake data

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,15 +7,15 @@ class User
     def self.fake_user_data
       @@data ||= (1..100).map do
         {
-          id: Faker::Lorem.characters(64),
+          id: Faker::Lorem.characters(16),
           favorite_count: Faker::Number.number(3),
           friend_count: Faker::Number.number(3),
           follower_count: Faker::Number.number(3),
           status_count: Faker::Number.number(3),
           listed_count: Faker::Number.number(3),
           name: Faker::Name.name,
-          screenName: Faker::Internet.user_name,
-          city: Faker::Address.city,
+          screen_name: Faker::Internet.user_name,
+          location: Faker::Address.city,
           language: "en"
         }
       end
