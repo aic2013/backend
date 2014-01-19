6c42db0088068c9b00fb6b94c9ee1efaef516045
Markus Mühlberger <mmuehlberger@me.com>
2014-01-19 13:27:14 +0100

    Add endpoints for users

--- app/api/aic13/users.rb
+++ app/api/aic13/users.rb
@@ -1,3 +1,29 @@
-class AIC13::Config < Grape::API
+class AIC13::Users < Grape::API
+  resource :users do
+    desc "Returns valid connection types for further use"
+    params do
+      required :topics, type: Array, desc: 'List of topic names'
+      optional :depth, type: Integer, desc: 'Search depth. Default: 2 (Friends of friends)'
+    end
+    get :topics do
+      [
+        { key: 'follows', value: 'Follows' },
+        { key: 'friends', value: 'Friends' },
+        { key: 'favorites', value: 'Favorites' },
+        { key: 'tweets', value: 'Tweets' },
+        { key: 'retweets', value: 'Retweets' }
+      ]
+    end
 
+    desc 'Configures variables for determining interesting ads'
+    params do
+      optional :connection_types, type: Array, desc: 'List of types to consider. Default: all'
+      optional :min_range, type: Float, desc: 'Minimum topic variety. Default: 0.0'
+      optional :max_range, type: Float, desc: 'Maximum topic variety. Default: 1.0'
+      optional :depth, type: Integer, desc: 'Search depth. Default: 2 (Friends of friends)'
+    end
+    get :suggestions do
+
+    end
+  end
 end
